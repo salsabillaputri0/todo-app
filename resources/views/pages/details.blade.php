b093486c5792a818760baacebbebf90154680373
salsabillaputri0 <2223612.salsabilla@smkn-2sbg.sch.id>
2025-02-11 15:23:00 +0700

    changes

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -1,12 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div id ="content" class="container">
+<div id ="content" class="container pb-3">
     <div class="d-flex align-items-center justify-content center">
-        <button class="btn btn-sm fw-bold fs-4">
+        <a href="{{route('home')}}" class="btn btn-sm fw-bold fs-4">
             <i class="bi bi-arrow-left-short"></i>
             Kembali
-        </button>
+        </a>
+    </div>
+
+    <div class="row">
+        <div class="col-8">
+            <div class="card" style="height: 80vh; max-height: 80vh;">
+                <div class="card-header d-flex align-items-center justify-content-between overflow-hidden">
+                    <h3 class=" fw-bold fs-4 text-truncate" style="max-width: 80%">{{$task->name}}</h3>
+                    <button class="btn btn-sm">
+                    <i class="bi bi-pencil-square fs-4"></i>
+                </button>
+                </div>
+                <div class="card-body">
+                    <p>
+                        {{$task->description}}
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-outline-danger w-100">
+                        Hapus
+                    </button>
+                </div>
+                
+                {{-- <button class="btn btn-sm" 
+                data-bs-toggle="modal" 
+                data-bs-target="#addListModal">
+                <i class="bi bi-pencil-square"></i>
+                </button>  --}}
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card" style="height: 80vh; max-height: 80vh;">
+
+            </div>
+            <h1>Tes</h1>
+        </div>
     </div>
 </div>
 @endsection
