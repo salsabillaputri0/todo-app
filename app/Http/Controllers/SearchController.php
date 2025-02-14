7f956b082e5f3bb9a153dc0c421e17a8aee5c420
salsabillaputri0 <2223612.salsabilla@smkn-2sbg.sch.id>
2025-02-14 11:00:48 +0700

    menambahkan perubahan

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -41,3 +41,4 @@ class SearchController extends Controller
         ]);
     }
 }
+
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,17 +2,19 @@
   - Menggunakan kelas navbar-expand-lg untuk membuat navbar responsif di berbagai ukuran layar. 
   - Memiliki warna latar belakang abu-abu (bg-secondary) dan teks putih. -->
 <nav class="navbar navbar-expand-lg bg-secondary navbar-dark ">
-    <div class="container d-flex justify-content-between">
+    <div class="container d-flex justify-content-end">
 
         <!-- Formulir pencarian untuk mencari data -->
+        
         <form class="d-flex" action="{{route('search')}}" method="GET" role="search">
             <!-- Input pencarian dengan id "search-input" -->
             <input id="search-input" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <!-- Tombol untuk melakukan pencarian atau refresh hasil pencarian 
              Formulir dengan input untuk mencari data, yang memungkinkan pengguna untuk mencari dan kemudian menampilkan hasil pencarian. Tombol bertuliskan "Refresh" digunakan untuk menyegarkan pencarian.-->
+             <div class="card-body">
             <button class="btn btn-outline-black" type="submit">Refresh</button>
         </form>
-        
+    </div>
         <!-- Logo aplikasi, diambil dari konfigurasi 'app.name' 
         Nama aplikasi diambil dari konfigurasi Laravel (config('app.name')), dan akan ditampilkan sebagai nama brand di navbar.-->
         <a class="navbar-brand fw-bolder" href="#">{{ config('app.name') }}</a>
